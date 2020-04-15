70e33793dd9011851b828b5cc16fc0ffc3d84406
ascatox <antonio.scatoloni@gmail.com>
2020-04-15 16:08:30 +0200

    New version now works with Endpoint A e B

--- src/main/java/it/eng/idsa/businesslogic/configuration/WebSocketServerConfigurationB.java
+++ src/main/java/it/eng/idsa/businesslogic/configuration/WebSocketServerConfigurationB.java
@@ -14,7 +14,7 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 public class WebSocketServerConfigurationB implements WebSocketServerConfiguration {
 
-    @Value("${communication.ws.endpointB.port}") //TODO
+    @Value("${communication.ws.endpointB.port}")
     private int port;
 
     @Bean(name="frameBufferWebSocketB")
