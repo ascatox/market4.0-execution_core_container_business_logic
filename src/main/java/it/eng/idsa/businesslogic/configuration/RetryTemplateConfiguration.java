25cd5c7299215c03e20b6dbb5a4baf45f67c1cf1
ascatox <antonio.scatoloni@gmail.com>
2020-05-27 16:57:12 +0200

    Travis introduction.

---
+++ src/main/java/it/eng/idsa/businesslogic/configuration/RetryTemplateConfiguration.java
@@ -0,0 +1,30 @@
+package it.eng.idsa.businesslogic.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
+
+/**
+ * @author Antonio Scatoloni on 05/05/2020
+ **/
+
+@Configuration
+public class RetryTemplateConfiguration {
+
+
+    @Bean
+    public RetryTemplate retryTemplate() {
+        RetryTemplate retryTemplate = new RetryTemplate();
+
+        FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+        fixedBackOffPolicy.setBackOffPeriod(5000);
+        retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
+
+        SimpleRetryPolicy retryPolicy = new SimpleRetryPolicy();
+        retryPolicy.setMaxAttempts(5);
+        retryTemplate.setRetryPolicy(retryPolicy);
+        return retryTemplate;
+    }
+}
