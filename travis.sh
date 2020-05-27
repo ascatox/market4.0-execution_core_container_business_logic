087b374a84ce7254f663d992ef572576f61ed0e5
ascatox <antonio.scatoloni@gmail.com>
2020-05-27 15:56:53 +0200

    Travis introduction.

--- travis.sh
+++ travis.sh
@@ -6,11 +6,21 @@ chmod +x docker-compose
 sudo mv docker-compose /usr/local/bin
 echo "docker-compose correctly installed"
 
+cp -f settings.xml $HOME/.m2/settings.xml
+
+echo "Installing websocket-message-streamer-lib..."
+git clone https://github.com/ascatox/websocket-message-streamer.git
+cd websocket-message-streamer
+mvn clean install
+cd ..
+echo "Installed websocket-message-streamer-lib!"
+
 echo "Cloning and Dockerizing Data-App repo..."
 git clone https://github.com/ascatox/market4.0-data_app_test_BE.git
 cd market4.0-data_app_test_BE
 git checkout master
 sh dockerize.sh
+cd ..
 
 echo "Dockerizing ECCs..."
 mvn clean package -DskipTests
