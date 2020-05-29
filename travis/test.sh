9abbabaf735c29d06f35f7155fac003fe857bd24
ascatox <antonio.scatoloni@gmail.com>
2020-05-29 08:35:30 +0200

    Travis staging first attemp.

--- travis/test.sh
+++ travis/test.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
 newman run ./travis/tests/tests-${NETWORK}.json --insecure
-
-#docker logs market40-execution_core_container_business_logic_data-app_1
-#docker logs market40-execution_core_container_business_logic_ecc-consumer_1
-#docker logs market40-execution_core_container_business_logic_ecc-provider_1
