a6c6dafb26d138e7887f8f339462747e9781e7c8
ascatox <antonio.scatoloni@gmail.com>
2020-06-03 14:38:11 +0200

    Removed stress-tests with naive-stress-test.sh.

--- travis/test.sh
+++ travis/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export CONCURR=1
 export REPS=20
-newman run ./travis/tests/stress-tests/tests-${NET}-${NETE}.json --insecure
+newman run ./travis/tests/tests-${NET}-${NETE}.json --insecure
 #bash ./travis/tests/stress-tests/tests-${NET}-${NETE}.sh
