fab10b45ddbba4e9f47c4cafd51e8827fe6cda0b
ascatox <antonio.scatoloni@gmail.com>
2020-06-03 10:47:12 +0200

    Travis introduction 4th phase.

--- travis/install.sh
+++ travis/install.sh
@@ -25,7 +25,7 @@ sudo mv docker-compose /usr/local/bin
 echo "docker-compose correctly installed"
 
 echo "REMOVE ASAP-> Settings.xml copy on Travis Home_M2 at: "$HOME
-cp -f ./travis/.m2/settings.xml $HOME/.m2/settings.xml
+#cp -f ./travis/.m2/settings.xml $HOME/.m2/settings.xml
 #cp -rf ./travis/.m2/repository  $HOME/.m2/
 echo "REMOVE ASAP-> Settings.xml copy on Travis Home_M2 at: "$HOME
 
