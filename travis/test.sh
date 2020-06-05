ff14a5598207ec0f973a8249098916a499f6ff17
ascatox <antonio.scatoloni@gmail.com>
2020-06-05 11:27:30 +0200

    Removed TPM_HOST

--- travis/test.sh
+++ travis/test.sh
@@ -2,4 +2,4 @@
 # infos at https://learning.postman.com/docs/postman/collection-runs/command-line-integration-with-newman/
 export TEST_TIMEOUT=120000 #2 min in ms
 export ITERATIONS=1
-newman run ./travis/tests/tests-${NET}-${NETE}.json --insecure --timeout-request ${TEST_TIMEOUT} --iteration-count ${ITERATIONS}
+newman run ./travis/tests/tests-${NET}-${NETE}.json --insecure --timeout-request ${TEST_TIMEOUT} --iteration-count ${ITERATIONS} --bail
