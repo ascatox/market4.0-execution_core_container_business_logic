90b30ce558e3270a352f2d88f4fdd8efacadef6a
ascatox <antonio.scatoloni@gmail.com>
2020-06-15 09:23:27 +0200

    Travis update

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo Deploy
 #docker login --username=$DOCKER_USER --password=$DOCKER_PASS $DOCKER_HOST
-#docker push market4.0/execution_core_container_business
+#docker push rdlabengpa/execution_core_container_bl
